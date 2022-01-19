42f8b79ba8eb222c88e619e468e483d5a54eb61d
hafizhMo <hafizh.hizb@gmail.com>
2022-01-19 22:03:57 +0700

    finalization

--- app/src/main/java/com/hafizhmo/crudtodolistapp/MainActivity.kt
+++ app/src/main/java/com/hafizhmo/crudtodolistapp/MainActivity.kt
@@ -3,14 +3,54 @@ package com.hafizhmo.crudtodolistapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.hafizhmo.crudtodolistapp.ui.add_edit_todo.AddEditTodoScreen
 import com.hafizhmo.crudtodolistapp.ui.theme.CRUDTodoListAppTheme
+import com.hafizhmo.crudtodolistapp.ui.todo_list.TodoListScreen
+import com.hafizhmo.crudtodolistapp.util.Routes
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CRUDTodoListAppTheme {
-
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Routes.TODO_LIST
+                ) {
+                    composable(Routes.TODO_LIST) {
+                        TodoListScreen(
+                            onNavigate = {
+                                navController.navigate(it.route)
+                            }
+                        )
+                    }
+                    composable(
+                        route = Routes.ADD_EDIT_TODO + "?todoId={todoId}",
+                        arguments = listOf(
+                            navArgument(name = "todoId") {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            }
+                        )
+                    ) {
+                        AddEditTodoScreen(onPopBackStack = {
+                            navController.popBackStack()
+                        })
+                    }
+                }
             }
         }
     }
